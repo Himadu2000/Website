8c9f2b9ab03234201ad4cc9a062a54773367bad1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-13 22:06:19 +0700

    Update home.tsx

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,10 +1,10 @@
 import { MDBContainer } from "mdbreact"
 import React from "react"
-import Comment from "./components//Comment/Comment"
 // import QuoteCard from "./components/Quote/QuoteCard";
 import Alert from "./components/Alert"
 import Carousel from "./components/Carousel"
 import AddComment from "./components/Comment/AddComment"
+import Comment from "./components/Comment/Comment"
 import Quotes from "./components/Quote/Quotes"
 
 const Homepage = () => (
